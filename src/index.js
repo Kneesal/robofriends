5fb4f95bb602cbcead3b310f79994d0be2f0f4cb
Kneesal <114973713+Kneesal@users.noreply.github.com>
2022-11-21 08:24:27 +1300

    made CardList parent component that loops through the robots.js array of objects and creates all the cards instead of individually listing them

--- src/index.js
+++ src/index.js
@@ -3,22 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
-import { robots } from './robots'
-import Card from './Card'; // "./" means to access the file in this directory. 
+import { robots } from './robots' //have to destructure because the robots file is not "export default"
+import CardList from './CardList'; //Parent component of card 
+ // "./" means to access the file in this directory. 
 // In some of the above there is no"./" and react knows to find the file outside of this directory
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <Card id = {robots[0].id} name = {robots[0].name} email = {robots[0].email}/>
-      <Card id = {robots[1].id} name = {robots[1].name} email = {robots[1].email}/>
-      <Card id = {robots[2].id} name = {robots[2].name} email = {robots[2].email}/>
-      <Card id = {robots[4].id} name = {robots[4].name} email = {robots[4].email}/>
-      <Card id = {robots[5].id} name = {robots[5].name} email = {robots[5].email}/>
-      <Card id = {robots[6].id} name = {robots[6].name} email = {robots[6].email}/>
-      <Card id = {robots[7].id} name = {robots[7].name} email = {robots[7].email}/>
-      <Card id = {robots[8].id} name = {robots[8].name} email = {robots[8].email}/>
-      <Card id = {robots[9].id} name = {robots[9].name} email = {robots[9].email}/>
+    <CardList robots = {robots}/>
   </React.StrictMode>
 );
 
